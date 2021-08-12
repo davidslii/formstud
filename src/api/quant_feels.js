9f8bcb201c418c6f78fac7a4424a0d4b9d051e16
davidslii <34230151+davidslii@users.noreply.github.com>
2021-08-11 17:25:01 -0700

    /src/pages/index.js

---
+++ src/api/quant_feels.js
@@ -0,0 +1,52 @@
+const Airtable = require("airtable")
+
+Airtable.configure({
+  endpointUrl: "https://api.airtable.com",
+  //Your API Key from Airtable
+  apiKey: process.env.AIRTABLE_KEY,
+})
+
+// Your Table ID from Airtable
+const db = Airtable.base(process.env.AIRTABLE_DB)
+
+const handler = (req, res) => {
+  try {
+    if (req.method !== "POST") {
+      return res.status(404).json({ message: "This endpoint requires a POST" })
+    }
+
+    const data = req.body
+
+    if (!data) {
+      return res.status(500).json({ error: "There isn't any data." })
+    }
+
+    db("Quantifit").create(
+      [
+        {
+          fields: {
+            Workout_Name: data.name,
+            Date: data.date,
+            Knees: data.message,
+            Back: data.message,
+          },
+        },
+      ],
+      (err, records) => {
+        if (err) {
+          res.json({
+            message: "Error adding record to Airtable.",
+            error: err.message,
+          })
+        } else {
+          res.json({ message: `Successfully submitted message` })
+        }
+      }
+    )
+  } catch (err) {
+    console.log(err)
+    res.json({ message: "There has been a big error.", error: err })
+  }
+}
+
+module.exports = handler
