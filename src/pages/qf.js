2fd36d68e851db074322d39b490d10dbdc22ed12
davidslii <34230151+davidslii@users.noreply.github.com>
2021-08-13 19:18:50 -0700

    /src/pages/index.js

---
+++ src/pages/qf.js
@@ -0,0 +1,36 @@
+import * as React from "react"
+
+export default function AirtableUI() {
+  return (
+    <form action="/api/qf" method="POST">
+      <h2 style={{ marginBottom: `16px` }}>How do you feel?</h2>
+      <div style={{ marginBottom: `8px` }}>
+        <label style={{ display: `block`, marginBottom: `4px` }} htmlFor="name">
+          Date:
+        </label>
+        <input name="date" id="date" type="date" />
+      </div>
+      <div style={{ marginBottom: `8px` }}>
+        <label
+          style={{ display: `block`, marginBottom: `4px` }}
+          htmlFor="muscle"
+        >
+          Muscle:
+        </label>
+        <input name="muscle" id="muscle" />
+      </div>
+      <div style={{ marginBottom: `24px` }}>
+        <label
+          style={{ display: `block`, marginBottom: `4px` }}
+          htmlFor="status"
+        >
+          Status (out of 10):
+        </label>
+        <textarea name="Status" id="status" />
+      </div>
+      <div>
+        <button>Submit</button>
+      </div>
+    </form>
+  )
+}
